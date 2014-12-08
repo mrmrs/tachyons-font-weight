83d8a2c2789fbb4a6c2a175e4f7e03a5ead7f624
mrmrs <hi@mrmrs.cc>
2014-12-08 15:34:07 -0800

    Update module to work.

--- gulpfile.js
+++ gulpfile.js
@@ -11,8 +11,8 @@ var gulp = require('gulp'),
 
 // Task that compiles scss files down to good old css
 gulp.task('pre-process', function(){
-  gulp.src('./font-weight.scss')
-      .pipe(watch('./font-weight.scss', function(files) {
+  gulp.src('./font_weight.scss')
+      .pipe(watch('./font_weight.scss', function(files) {
         return files.pipe(sass())
           .pipe(size({gzip: false, showFiles: true, title:'un-prefixed css'}))
           .pipe(size({gzip: true, showFiles: true, title:'un-prefixed gzipped css'}))
@@ -21,7 +21,7 @@ gulp.task('pre-process', function(){
           .pipe(size({gzip: true, showFiles: true, title:'prefixed css'}))
           .pipe(gulp.dest('./'))
           .pipe(minifyCSS())
-          .pipe(rename('font-weight.min.css'))
+          .pipe(rename('font_weight.min.css'))
           .pipe(gulp.dest('./'))
           .pipe(size({gzip: false, showFiles: true, title:'minified css'}))
           .pipe(size({gzip: true, showFiles: true, title:'minified css'}))
